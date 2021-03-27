1eea5a9d5674991fe22c48833ad1f1ab9d31220a
schei <schei@LAPTOP-5K7P037U>
2021-03-27 11:23:51 -0400

    Abstract Classes, should be good, may change as we go though

--- src/items/Bar.java
+++ src/items/Bar.java
@@ -1,5 +1,13 @@
 package items;
 
-public abstract class Bar extends Item {
-
+public abstract class Bar extends Item 
+{
+	//apathom is the "radius" of a uniform shape
+	//equivalent to the radius of a circle
+	private double apathom;
+	
+	public abstract double getApathom();
+	
+	//area for any shape is .5 * apathom * perimeter, specified in particular class
+	public abstract double getArea();
 }
--- src/items/Item.java
+++ src/items/Item.java
@@ -1,5 +1,15 @@
 package items;
 
-public abstract class Item implements Material{
-
+public abstract class Item implements Material
+{
+	//updates values of the item
+	public abstract void update();
+	
+	//display for GUI
+	public abstract void display();
+	
+	//length of the material
+	private double length;
+	
+	public abstract double getLength();
 }
