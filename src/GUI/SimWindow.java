848bd9c40763b39d2f660b6203449b84e9cc3137
ElijahK01 <70280997+ElijahK01@users.noreply.github.com>
2021-03-27 16:25:51 -0400

    Added getForce method in SimWindow

--- src/GUI/SimWindow.java
+++ src/GUI/SimWindow.java
@@ -1,5 +1,18 @@
 package GUI;
 
-public class SimWindow {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class SimWindow extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	public double getForce() {
+		return 1.00; // returns value for user input
+	}
 }
